658e665e6a9306fc56d7a747071f3509c05a5832
arsh98000 <arsh98000@gmail.com>
2018-11-04 18:02:32 -0600

    Added the province model.

---
+++ db/migrate/20181105000131_create_provinces.rb
@@ -0,0 +1,12 @@
+class CreateProvinces < ActiveRecord::Migration[5.2]
+  def change
+    create_table :provinces do |t|
+      t.string :name
+      t.string :province_abbr
+      t.decimal :gst
+      t.decimal :pst
+
+      t.timestamps
+    end
+  end
+end
