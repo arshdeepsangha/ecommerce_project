3f78aee49f920bb86f0a7a11221e94a186c4c0ae
arsh98000 <arsh98000@gmail.com>
2018-11-05 13:28:45 -0600

    Added the contact page record to DB .

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
 
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
 
   resources :vehicles, only:[:index, :show]
   resources :pages ,only:[:show]
@@ -12,8 +14,6 @@ Rails.application.routes.draw do
   root to: 'vehicles#index'
 
 
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   
