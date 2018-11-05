57f9afd127ee93c6c1a578e8023c9c0a4bb0163b
arsh98000 <arsh98000@gmail.com>
2018-11-04 18:22:13 -0600

    Added the Order model.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_05_001817) do
+ActiveRecord::Schema.define(version: 2018_11_05_002128) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
@@ -37,6 +37,16 @@ ActiveRecord::Schema.define(version: 2018_11_05_001817) do
     t.string "deal_rate_decimal"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "orders", force: :cascade do |t|
+    t.integer "quantity"
+    t.integer "vehicle_id"
+    t.integer "customer_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["customer_id"], name: "index_orders_on_customer_id"
+    t.index ["vehicle_id"], name: "index_orders_on_vehicle_id"
   end
 
   create_table "provinces", force: :cascade do |t|
