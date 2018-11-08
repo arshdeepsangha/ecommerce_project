959c7f936aea784ebad8c80e0c563335aab13ae2
arsh98000 <arsh98000@gmail.com>
2018-11-07 22:03:08 -0600

    Added Sign Up functionality for visitors to website for now being classified as customers and purchase vehicles.

--- app/controllers/customer_controller.rb
+++ app/controllers/customer_controller.rb
@@ -1,11 +1,24 @@
 class CustomerController < ApplicationController
   def signup
     @customer = Customer.new
+    @provinces = Province.all
+    
+  end
+
+  def create
+    @customer = Customer.create(customer_params)
+    @customer.save
+    flash[:notice]="You have signed up successfully.."
+    redirect_to root_path
   end
 
   def signin
   end
 
   def signout
+  end
+
+  def customer_params
+    params.require(:customer).permit(:username,:password,:firstname,:lastname,:address,:city,:email,:province_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
 
   get 'customer/signup'
+  post 'customer/signup' => 'customer#create'
   get 'customer/signin'
   get 'customer/signout'
   devise_for :admin_users, ActiveAdmin::Devise.config
